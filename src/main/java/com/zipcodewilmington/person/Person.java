432edaad00532d9c5338b354909e7bcb80c4211c
deon <deon@zipcodes-MacBook-Pro-23.local>
2020-11-07 16:06:09 -0500

    Finishing a Person

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,66 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String color;
+    private boolean hungry;
+    private String car;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
-    }
-
-    public void setName(String name) {
-    }
-
-    public void setAge(int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
-    public Integer getAge() {
-        return null;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isHungry() {
+        return hungry;
+    }
+
+    public void setHungry(boolean hungry) {
+        this.hungry = hungry;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
     }
 }
+
